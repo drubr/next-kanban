3b4496e739ab3dff686a627a62d72e0719273ac9
Dominik Rubröder <dominik.rubroeder@mediawave.de>
2024-02-16 16:07:59 +0100

    Start building Desktop UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,92 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex h-screen">
+      <aside className="h-full w-[14rem] shrink-0 select-none gap-4 border-r border-slate-800 bg-slate-900 pt-6">
+        <div className="mx-4 h-8 rounded-full bg-slate-700"></div>
+
+        <div className="h-8"></div>
+
+        <div className="text-muted mb-2 pl-6 text-xs uppercase tracking-wider">
+          All boards
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <ul className="grid gap-1 pr-6">
+          <li>
+            <div className="flex items-center gap-2 rounded-r-full bg-violet-600 py-2 pl-6 text-sm">
+              <span className="inline-block h-4 w-4 rounded bg-white"></span>
+              Platform Launch
+            </div>
+          </li>
+          <li>
+            <div className="text-muted flex items-center gap-2 rounded-r-full bg-transparent py-2 pl-6 text-sm">
+              <span className="bg-muted inline-block h-4 w-4 rounded"></span>
+              Marketing Plan
+            </div>
+          </li>
+          <li>
+            <div className="text-muted flex items-center gap-2 rounded-r-full bg-transparent py-2 pl-6 text-sm">
+              <span className="bg-muted inline-block h-4 w-4 rounded"></span>
+              Roadmap
+            </div>
+          </li>
+          <li>
+            <div className="flex items-center gap-2 rounded-r-full bg-transparent py-2 pl-6 text-sm text-violet-400">
+              <span className="inline-block h-4 w-4 rounded bg-violet-400"></span>
+              + Create New Board
+            </div>
+          </li>
+        </ul>
+      </aside>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="flex-1 bg-slate-950">
+        <header className="flex items-center justify-between gap-4 border-b border-b-slate-800 bg-slate-900 px-8 py-4">
+          <h1 className="text-xl font-semibold">Platform Launch</h1>
+          <button
+            className="rounded-full bg-violet-600 px-4 py-3"
+            title="Add new task"
+            aria-label="Add new task button"
+          >
+            + Add new task
+          </button>
+        </header>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <section className="overflow-auto whitespace-nowrap p-4">
+          <div className="inline-block w-[16rem] space-y-6">
+            <div>Headline</div>
+            <ul className="space-y-4">
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+            </ul>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="inline-block w-[16rem] space-y-6">
+            <div>Headline</div>
+            <ul className="space-y-4">
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+            </ul>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="inline-block w-[16rem] space-y-6">
+            <div>Headline</div>
+            <ul className="space-y-4">
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+              <li className="h-16 rounded-lg bg-slate-800"></li>
+            </ul>
+          </div>
+        </section>
       </div>
     </main>
   );
